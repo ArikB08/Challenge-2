e4ed8c9a1d71e953e58bf919add2df34e4ca056b
Lai Hong Yu <joyjay606@gmail.com>
2025-08-16 18:59:34 +0800

    created ui

---
+++ Challenge 2/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Challenge 2
+//
+//  Created by Lai Hong Yu on 8/2/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
